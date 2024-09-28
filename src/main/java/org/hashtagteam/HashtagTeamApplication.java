3cae9360045580c0966ce959aef6823e32836bf7
ls <verde07e@gmail.com>
2024-09-28 16:21:54 +0900

    [초기설정]

---
+++ src/main/java/org/hashtagteam/HashtagTeamApplication.java
@@ -0,0 +1,13 @@
+package org.hashtagteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashtagTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashtagTeamApplication.class, args);
+	}
+
+}
